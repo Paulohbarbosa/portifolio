24efa6aef8c52ca15f824ba229f068c7d3e8486a
Paulo Barbosa <paulobarbosah@hotmail.com>
2026-09-02 10:30:08 -0300

    feat: create Navbar component with navigation links and contact button

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,14 +2,26 @@ export default function Navbar() {
   return (
     <nav className="navbar">
       <div className="nav-container">
-        <div className="logo">Paulo<span className="text-blue">Barbosa</span></div>
+        <a href="#" className="logo">
+          Paulo<span className="text-blue">Barbosa</span>
+        </a>
         <ul className="nav-links">
-          <li><a href="#skills">Stack</a></li>
-          <li><a href="#experiencia">Experiência</a></li>
-          <li><a href="#projetos">Projetos</a></li>
-          <li><a href="#depoimentos">Depoimentos</a></li>
+          <li>
+            <a href="#skills">Stack</a>
+          </li>
+          <li>
+            <a href="#experiencia">Experiência</a>
+          </li>
+          <li>
+            <a href="#projetos">Projetos</a>
+          </li>
+          <li>
+            <a href="#depoimentos">Depoimentos</a>
+          </li>
         </ul>
-        <a href="#contato" className="btn-nav">Iniciar Projeto</a>
+        <a href="#contato" className="btn-nav">
+          Iniciar Projeto
+        </a>
       </div>
     </nav>
   );
